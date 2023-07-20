5a26146d51d6ac80d115102a01b005cd206abe04
mrcelo014@gmail.com <mrcelo014@gmail.com>
2023-07-19 21:37:42 -0300

    Adicionando tarefas

--- main.js
+++ main.js
@@ -1,28 +1,87 @@
-const addTask = () => {
-  const inputTask = document.querySelector('#input-task')
-  createTask(inputTask)
+const tbody = document.querySelector('tbody')
+const addTask = document.querySelector('.add-form')
+
+addTask.addEventListener('submit', e => {
+  e.preventDefault()
+  textTask()
+  createRow()
+})
+
+const createElement = (tag) => document.createElement(tag)
+
+const createSelect = () => {
+  const options = `
+  <option value="pendente">pendente</option>
+  <option value="em andamento">em andamento</option>
+  <option value="concluido">concluido</option>
+  `
+  const select = createElement('select')
+  select.innerHTML = options
+  return select
 }
 
-const createElement = (tag) => {
-  return document.createElement(tag)
+const createTextBtn = () => {
+  const spanEdit = `
+  <span class="material-symbols-outlined">edit</span>
+  `
+  const spanDelete = `
+  <span class="material-symbols-outlined">delete</span>
+  `
+  return [spanEdit, spanDelete]
 }
 
-const createTask = (text) => {
-  const tbody = document.querySelector('tbody')
+const createRow = (task) => {
+  const textTask = localStorage.getItem('textOfTask')
+  const dateTask = localStorage.getItem('dateTask')
+  
+  const select = createSelect()
+  const [spanEdit, spanDelete] = createTextBtn()
   
   const tr = createElement('tr')
-  const td = createElement('td')
+  const tdTitle = createElement('td')
+  const tdDate = createElement('td')
+  const tdStatus = createElement('td')
+  const tdBtn = createElement('td')
+  const btnEdit = createElement('button')
+  const btnDelete = createElement('button')
   
-  tbody.appendChild(tr)
+  btnEdit.classList.add('btn-action')
+  btnDelete.classList.add('btn-action')
   
-  td
+  tdTitle.innerHTML = textTask
+  tdDate.innerHTML = dateTask
+  btnEdit.innerHTML = spanEdit
+  btnDelete.innerHTML = spanDelete
   
+  if (textTask) {
+    tbody.appendChild(tr)
+    tdStatus.appendChild(select)
+    tdBtn.appendChild(btnEdit)
+    tdBtn.appendChild(btnDelete)
+    
+    tr.appendChild(tdTitle)
+    tr.appendChild(tdDate)
+    tr.appendChild(tdStatus)
+    tr.appendChild(tdBtn)
+  }
+}
+createRow()
+
+const textTask = () => {
+  const textTask = document.querySelector('#input-task').value
+  
+  if (!textTask) return alert('Digite algo antes de adicionar!')
+  
+  localStorage.setItem('textOfTask', textTask)
 }
 
 const createDate = () => {
   const date = new Date()
-  return date.toLocaleString()
+  localStorage.setItem('dateTask', date.toLocaleString()) 
 }
+createDate()
+
+
 
 
 
